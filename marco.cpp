9922c5f9fe8ca9acc84902c0f1f1f9437972bf00
jesuemmanuel <jesutlaltecatl@gmail.com>
2024-06-11 15:23:00 -0600

    Add files via upload

---
+++ marco.cpp
@@ -0,0 +1,30 @@
+#include using namespace std;
+
+int main() { // Solicitar el título y el carácter al usuario string titulo; char caracter;
+
+cout << "Ingrese el título: ";
+getline(cin, titulo);
+
+cout << "Ingrese el carácter de enmarcado: ";
+cin >> caracter;
+
+// Determinar el ancho del marco
+int ancho = titulo.length() + 4;
+
+// Imprimir el marco superior
+for (int i = 0; i < ancho; i++) {
+    cout << caracter;
+}
+cout << endl;
+
+// Imprimir el título enmarcado
+cout << caracter << " " << titulo << " " << caracter << endl;
+
+// Imprimir el marco inferior
+for (int i = 0; i < ancho; i++) {
+    cout << caracter;
+}
+cout << endl;
+
+return 0;
+}
